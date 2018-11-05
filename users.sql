0fa3b0733b1d27dea01c9c1127292557ea7e460e
JerryKristanto <jerrykristanto4234@gmail.com>
2018-11-05 10:39:19 +0700

    first commit

---
+++ users.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2018 at 04:30 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `api-php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `level` varchar(100) NOT NULL,
+  `fullname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `level`, `fullname`) VALUES
+(22, 'Jerry Kristanto', 'jerry', 'Admin', 'Jerry Kristanto'),
+(23, 'Joya Nafal Ulya', 'joya123', 'Admin', 'Joya Nafal Ulya'),
+(24, 'Julia Dwi Ana', 'julia123', 'Admin', 'Julia Dwi Ana'),
+(25, 'Marzandi Zahran Affandi L', 'marzandiea', 'Admin', 'Marzandi Zahran Affandi L'),
+(26, 'Muhammad Fikri Nabil Assyawali', 'fikri123', 'Admin', 'Muhammad Fikri Nabil Assyawali');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
